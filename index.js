8bfaeb430458cf7f20d4e9d21af530ef109f9cda
NafNgeru <59029279+NafNgeru@users.noreply.github.com>
2024-12-20 11:30:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-
-const filmList = document.getElementById("films")
+//Creating variables for DOM manipulation
+const filmList = document.getElementById("films") //Variable created for the placeholder's content 
 
 const poster = document.getElementById("poster")
 const title = document.getElementById("title")
@@ -7,8 +7,9 @@ const runtime = document.getElementById("runtime")
 const showtime = document.getElementById("showtime")
 const availableTickets = document.getElementById("available-tickets")
 
-const buyTicketButton = document.getElementById("buy-ticket")
+const buyTicketButton = document.getElementById("buy-ticket") //Defining button variable
 
+//Fetch function to get data from the json
 function fetchMovies(){
     fetch("http://localhost:3000/films")
     .then(response => response.json())
@@ -18,17 +19,23 @@ function fetchMovies(){
     })
 }
 
+//Functions to render the films on the menu list 
 function renderFilms(films){
     filmList.innerHTML = ""; //Clear the placeholder
+    //Render for each film
     films.forEach((film) => {
         const filmElement = document.createElement("li");
         filmElement.textContent = film.title;
         filmElement.className = "film item";
+
+        //Event listener for clicking the films on the menu list
         filmElement.addEventListener("click", () => showMovie(film));
         filmList.appendChild(filmElement);
     });
 };
 
+//Function to access the film elements from the DOM variables created
+//Function to show the film
 function showMovie(film){
     poster.src = film.poster;
     title.textContent = film.title;
@@ -36,17 +43,20 @@ function showMovie(film){
     showtime.textContent = film.showtime;
     availableTickets.textContent = film.capacity - film.tickets_sold;
 
+    //On click function for when the button click event is done
     buyTicketButton.onclick = function(){
         buyTicket(film);
     };
 }
 
+//Function to calculate number of tickets, patch changes to json file, and render them on the front-end
 const buyTicket = function(film){
     const tickets = film.capacity - film.tickets_sold;
     if (tickets > 0){
         film.tickets_sold++;
         availableTickets.textContent = film.capacity - film.tickets_sold;
 
+        //Patch the changes in available tickets
         fetch(`http://localhost:3000/films/${film.id}`, {
             method: "PATCH",
             headers: {
@@ -55,10 +65,12 @@ const buyTicket = function(film){
             body: JSON.stringify({ tickets_sold: film.tickets_sold }),
         });
     } else {
+        //Tell the user when the tickets are all sold out
         alert("All Tickets have been sold. Wanna tyr another movie?");
     }
 }
 
+//Load the DOM from the accessed data
 document.addEventListener("DOMContentLoaded", () => {
     fetchMovies();
     console.log("Hurrraaaayyy");
